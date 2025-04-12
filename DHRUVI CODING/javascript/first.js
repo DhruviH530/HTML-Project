1473ccb8b3bdc6ed69bfbe9628ece16d86c56733
Dhruvi Desai <desaidhruvi345@gmail.com>
2025-04-12 21:14:58 +0530

    HTML PROJECT

---
+++ DHRUVI CODING/javascript/first.js
@@ -0,0 +1,13 @@
+const ages = [21 , 22, 23, 24, 25];
+const numberofpeople = ages.length;
+
+
+for (let i=0; i< numberofpeople; i++){
+    if (ages[i] % 2 == 0){
+        console.log(ages[i]);
+    }
+}
+
+
+
+    
